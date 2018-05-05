8f8b1849ca62d450a49924be22676ac09c5b7724
marilenavelonia <marvelonia@windowslive.com>
2018-05-05 18:34:34 +0300

    Update question2.py

--- question2.py
+++ question2.py
@@ -1,18 +1,20 @@
 import redis
 
+#connect to redis
 redis_cli = redis.StrictRedis(host="localhost", port=6379, db=0)
 
 keyslist = []
 
+#for every key
 for key in redis_cli.scan_iter():
     if key not in keyslist:
         keyslist.append(key)
         values = redis_cli.lrange(key, -100, -1)
         sum = 0
+        #for the last 100 values of the key
         for value in values:
             strvalue = value.decode('ascii')
             flovalue = float(strvalue)
-            #value1 = struct.unpack("<L", value)
             sum = sum + flovalue
         length = redis_cli.llen(key)
         avg = sum/length
