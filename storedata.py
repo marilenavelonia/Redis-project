d31cd28edce566a2f492fe5571cb44c5196e0c20
marilenavelonia <marvelonia@windowslive.com>
2018-05-05 18:31:47 +0300

    Update storedata.py

--- storedata.py
+++ storedata.py
@@ -3,14 +3,17 @@ import time
 import os
 import redis
 
+#connect to redis
 redis_cli = redis.StrictRedis(host="localhost", port=6379, db=0)
 
+#create socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 serverip = '46.4.18.132'
 port = 1337
 client_socket.connect((serverip, port))
 
+#continuously read from stream
 while 1:
     data = client_socket.recv(1024)
     data1 = data.decode('ascii')
@@ -21,4 +24,5 @@ while 1:
         data31 = j.split('\r\n')
         data4.extend(data31)
     for i in range(0, len(data4), 2):
+        #store data in redis
         redis_cli.rpush(data4[i], data4[i+1])
